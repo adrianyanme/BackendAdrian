2def40b831e0af9a36536d6055fa3062179fe41d
adrianyanme <nusa.adrian.20@student.se.ittelkom-sby.ac.id>
2024-07-05 20:59:13 +0700

    Base

---
+++ routes/web.php
@@ -0,0 +1,25 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Auth\PasswordResetController;
+use App\Http\Controllers\Auth\ResetPasswordController;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+
+Route::get('password/reset/{token}', [ResetPasswordController::class, 'showResetForm'])->name('password.reset');
+Route::post('password/reset', [ResetPasswordController::class, 'reset'])->name('password.update');
+Route::get('/password/reset/success', function () {return view('auth.passwords.success');})->name('password.reset.success');
