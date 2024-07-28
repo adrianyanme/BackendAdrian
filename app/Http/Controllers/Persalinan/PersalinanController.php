29e4545eead42021f7f28541777be2b74629257b
adrianyanme <nusa.adrian.20@student.se.ittelkom-sby.ac.id>
2024-07-29 05:30:23 +0700

    NEW UPDATE

--- app/Http/Controllers/Persalinan/PersalinanController.php
+++ app/Http/Controllers/Persalinan/PersalinanController.php
@@ -14,84 +14,6 @@ use App\Mail\Persalinan\Persalinan as PersalinanPersalinan;
 
 class PersalinanController extends Controller
 {
-    // public function store(Request $request)
-    // {
-    //     $validated = $request->validate([
-    //         'email' => 'required',
-    //         'jenissalinan' => 'required',
-    //         'putusanyangdiminta' => 'required',
-    //         'namapemohon' => 'required',
-    //         'nohp' => 'required',
-    //         'statuspemohon' => 'required',
-    //         'noperkara' => 'required',
-    //         'namaparapihak' => 'required',
-    //         'ktppemohon' => 'required|file|mimes:pdf,png,jpg,jpeg',
-    //         'kkpemohon' => 'required|file|mimes:pdf,png,jpg,jpeg',
-    //         'relaaspemberitahuanputusan' => 'required|file|mimes:pdf,png,jpg,jpeg',
-    //         'catatanpemohon' => 'required',
-    //     ]);
-
-    //     $ktppemohon = null;
-    //     if ($request->hasFile('ktppemohon')) {
-    //         $file = $request->file('ktppemohon');
-    //         if ($file->isValid()) {
-    //             $path = $file->store('public/ktppemohon');
-    //             $ktppemohon = Storage::url($path);
-    //         } else {
-    //             throw new \Exception('Invalid file uploaded for ktppemohon.');
-    //         }
-    //     }
-
-    //     $kkpemohon = null;
-    //     if ($request->hasFile('kkpemohon')) {
-    //         $file = $request->file('kkpemohon');
-    //         if ($file->isValid()) {
-    //             $path = $file->store('public/kkpemohon');
-    //             $kkpemohon = Storage::url($path);
-    //         } else {
-    //             throw new \Exception('Invalid file uploaded for kkpemohon.');
-    //         }
-    //     }
-
-    //     $relaaspemberitahuanputusan = null;
-    //     if ($request->hasFile('relaaspemberitahuanputusan')) {
-    //         $file = $request->file('relaaspemberitahuanputusan');
-    //         if ($file->isValid()) {
-    //             $path = $file->store('public/relaaspemberitahuanputusan');
-    //             $relaaspemberitahuanputusan = Storage::url($path);
-    //         } else {
-    //             throw new \Exception('Invalid file uploaded for relaaspemberitahuanputusan.');
-    //         }
-    //     }
-
-    //     $currentUser = Auth::user();
-    //     $data = $request->all();
-    //     $data['author'] = Auth::user()->id;
-    //     $data['status'] = 'pending';
-    //     $data['ktppemohon'] = $ktppemohon;
-    //     $data['kkpemohon'] = $kkpemohon;
-    //     $data['relaaspemberitahuanputusan'] = $relaaspemberitahuanputusan;
-
-    //     $persalinan = Persalinan::create($data);
-
-    //     Mail::to($request->email)->send(new PersalinanPersalinan($currentUser, $request->namapemohon, $request->noperkara));
-
-    //     return new PersalinanResource($persalinan->loadMissing(['writer:id,username']));
-    // }
-
-
-    public function index()
-    {
-        $user = Auth::user();
-
-        // Cek apakah pengguna adalah superadmin
-        if ($user->role != 'superadmin') {
-            return response()->json(['message' => 'You do not have permission to view this data'], 403);
-        }
-
-        $persalinan = Persalinan::all();
-        return PersalinanResource::collection($persalinan);
-    }
     public function store(Request $request)
     {
         $validated = $request->validate([
@@ -152,14 +74,92 @@ class PersalinanController extends Controller
 
         $persalinan = Persalinan::create($data);
 
-        // Kirim email
         Mail::to($request->email)->send(new PersalinanPersalinan($currentUser, $request->namapemohon, $request->noperkara));
-
-        // Kirim WhatsApp
-        $this->sendWhatsAppNotification($request->nohp, $request->namapemohon, $request->noperkara);
 
         return new PersalinanResource($persalinan->loadMissing(['writer:id,username']));
     }
+
+
+    public function index()
+    {
+        $user = Auth::user();
+
+        // Cek apakah pengguna adalah superadmin
+        if ($user->role != 'superadmin') {
+            return response()->json(['message' => 'You do not have permission to view this data'], 403);
+        }
+
+        $persalinan = Persalinan::all();
+        return PersalinanResource::collection($persalinan);
+    }
+    // public function store(Request $request)
+    // {
+    //     $validated = $request->validate([
+    //         'email' => 'required',
+    //         'jenissalinan' => 'required',
+    //         'putusanyangdiminta' => 'required',
+    //         'namapemohon' => 'required',
+    //         'nohp' => 'required',
+    //         'statuspemohon' => 'required',
+    //         'noperkara' => 'required',
+    //         'namaparapihak' => 'required',
+    //         'ktppemohon' => 'required|file|mimes:pdf,png,jpg,jpeg',
+    //         'kkpemohon' => 'required|file|mimes:pdf,png,jpg,jpeg',
+    //         'relaaspemberitahuanputusan' => 'required|file|mimes:pdf,png,jpg,jpeg',
+    //         'catatanpemohon' => 'required',
+    //     ]);
+
+    //     $ktppemohon = null;
+    //     if ($request->hasFile('ktppemohon')) {
+    //         $file = $request->file('ktppemohon');
+    //         if ($file->isValid()) {
+    //             $path = $file->store('public/ktppemohon');
+    //             $ktppemohon = Storage::url($path);
+    //         } else {
+    //             throw new \Exception('Invalid file uploaded for ktppemohon.');
+    //         }
+    //     }
+
+    //     $kkpemohon = null;
+    //     if ($request->hasFile('kkpemohon')) {
+    //         $file = $request->file('kkpemohon');
+    //         if ($file->isValid()) {
+    //             $path = $file->store('public/kkpemohon');
+    //             $kkpemohon = Storage::url($path);
+    //         } else {
+    //             throw new \Exception('Invalid file uploaded for kkpemohon.');
+    //         }
+    //     }
+
+    //     $relaaspemberitahuanputusan = null;
+    //     if ($request->hasFile('relaaspemberitahuanputusan')) {
+    //         $file = $request->file('relaaspemberitahuanputusan');
+    //         if ($file->isValid()) {
+    //             $path = $file->store('public/relaaspemberitahuanputusan');
+    //             $relaaspemberitahuanputusan = Storage::url($path);
+    //         } else {
+    //             throw new \Exception('Invalid file uploaded for relaaspemberitahuanputusan.');
+    //         }
+    //     }
+
+    //     $currentUser = Auth::user();
+    //     $data = $request->all();
+    //     $data['author'] = Auth::user()->id;
+    //     $data['status'] = 'pending';
+    //     $data['ktppemohon'] = $ktppemohon;
+    //     $data['kkpemohon'] = $kkpemohon;
+    //     $data['relaaspemberitahuanputusan'] = $relaaspemberitahuanputusan;
+
+    //     $persalinan = Persalinan::create($data);
+
+    //     // Kirim email
+    //     Mail::to($request->email)->send(new PersalinanPersalinan($currentUser, $request->namapemohon, $request->noperkara));
+
+    //     // Kirim WhatsApp
+    //     $this->sendWhatsAppNotification($request->nohp, $request->namapemohon, $request->noperkara);
+
+    //     return new PersalinanResource($persalinan->loadMissing(['writer:id,username']));
+    // }
 
     private function sendWhatsAppNotification($phoneNumber, $namaPemohon, $noPerkara)
     {
